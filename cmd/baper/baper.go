4b3e069257e2f366c4a1127c760b0f2bd6640450
Wuriyanto <wuriyanto48@yahoo.co.id>
2019-12-09 12:51:59 +0700

    update binary

--- cmd/baper/baper.go
+++ cmd/baper/baper.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/telkomdev/baper"
+)
+
+func main() {
+	sender := baper.StdoutSender{}
+
+	if err := baper.Collect(sender, 2); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
