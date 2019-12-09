e6d21ae7c1f70542b3a3b6a7661cf0b6a9828684
Wuriyanto <wuriyanto48@yahoo.co.id>
2019-12-09 12:51:22 +0700

    add parser test

---
+++ parser_test.go
@@ -0,0 +1,62 @@
+package baper
+
+import (
+	"testing"
+)
+
+func TestIsNumber(t *testing.T) {
+
+	t.Run("should return true with valid number string params", func(t *testing.T) {
+		res := isNumber("5")
+		if !res {
+			t.Error("error, should return true")
+		}
+	})
+
+	t.Run("should return false with invalid number string params", func(t *testing.T) {
+		res := isNumber("v")
+		if res == true {
+			t.Error("error, should return false")
+		}
+	})
+}
+
+func TestTrim(t *testing.T) {
+
+	input := " hello world   "
+	expected := "hello world"
+
+	t.Run("should success trim string", func(t *testing.T) {
+		res := trim(input)
+
+		if res != expected {
+			t.Error("error trim string")
+		}
+	})
+}
+
+func TestSplit(t *testing.T) {
+
+	input := " hello world   "
+	expected := []string{"hello", "world"}
+
+	t.Run("should success split string", func(t *testing.T) {
+		s := trim(input)
+		ss := split(s)
+
+		if expected[0] != ss[0] {
+			t.Error("error split string")
+		}
+	})
+}
+
+func TestParse(t *testing.T) {
+	input := "  318.44    9  2.79   4  1 95  3.02 2.01 1.73"
+
+	t.Run("should success parse string to stat", func(t *testing.T) {
+		cpuStat := Parse(input)
+		if cpuStat == nil {
+			t.Error("error parse input")
+		}
+	})
+}
