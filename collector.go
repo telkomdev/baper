a3c47b9e2d52a50e32b71b597b04b8d498a0a8ed
Wuriyanto <wuriyanto48@yahoo.co.id>
2019-12-09 12:51:32 +0700

    add collector

---
+++ collector.go
@@ -0,0 +1,66 @@
+package baper
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os/exec"
+	"strconv"
+)
+
+func Collect(sender Sender, interval int) error {
+	const IOStatCommand = "iostat"
+
+	intervalStr := strconv.Itoa(interval)
+
+	cmd := exec.Command(IOStatCommand, "-w", intervalStr)
+
+	outPipe, _ := cmd.StdoutPipe()
+	err := cmd.Start()
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("process running on PID = ", cmd.Process.Pid)
+
+	// time.AfterFunc(10*time.Second, func() {
+	// 	err = cmd.Process.Kill()
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	}
+	// 	fmt.Println("kill process")
+	// })
+
+	reader := bufio.NewReader(outPipe)
+
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				return err
+			}
+		}
+
+		cpuStat := Parse(line)
+		if cpuStat != nil {
+			payload, err := json.Marshal(cpuStat)
+			if err != nil {
+				fmt.Println("error marshal data : ", err.Error())
+			}
+
+			if err := sender.Send(payload); err != nil {
+				return err
+			}
+		}
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
