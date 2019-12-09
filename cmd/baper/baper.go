c507841c169dc2ef662f6cdf89c0a5ef397c5f9b
Wuriyanto <wuriyanto48@yahoo.co.id>
2019-12-09 13:05:19 +0700

    add cli flag

--- cmd/baper/baper.go
+++ cmd/baper/baper.go
@@ -1,16 +1,51 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
 	"github.com/telkomdev/baper"
 )
 
+// Version number
+const Version = "0.0.0"
+
 func main() {
+
+	var (
+		showVersion bool
+		interval    int
+	)
+
+	flag.IntVar(&interval, "interval", 1, "the interval between execution")
+	flag.IntVar(&interval, "i", 1, "the interval between execution")
+	flag.BoolVar(&showVersion, "version", false, "show version")
+	flag.BoolVar(&showVersion, "v", false, "show version")
+
+	flag.Usage = func() {
+		fmt.Println()
+		fmt.Println("Usage: ")
+		fmt.Println("baper -[options]")
+		fmt.Println()
+		fmt.Println("-i | -interval (the interval between execution)")
+		fmt.Println("-h | -help (show help)")
+		fmt.Println("-v | -version (show version)")
+		fmt.Println("---------------------------")
+		fmt.Println()
+	}
+
+	flag.Parse()
+
+	if showVersion {
+		fmt.Printf("  baper version %s\n", Version)
+		fmt.Println()
+		os.Exit(0)
+	}
+
 	sender := baper.StdoutSender{}
 
-	if err := baper.Collect(sender, 2); err != nil {
+	if err := baper.Collect(sender, interval); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
