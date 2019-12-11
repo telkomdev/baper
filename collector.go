ce4c21d2c1b6a6d4722da802c39447214ad46589
Wuriyanto <wuriyanto48@yahoo.co.id>
2019-12-11 22:03:48 +0700

    add collector struct

--- collector.go
+++ collector.go
@@ -9,29 +9,31 @@ import (
 	"strconv"
 )
 
-// Collect function will collect information from iostat
-func Collect(sender Sender, interval int) error {
+// Collector struct
+type Collector struct {
+	cmd    *exec.Cmd
+	sender Sender
+}
+
+// New function, Collector's constructor
+func New(sender Sender, interval int) *Collector {
 	const IOStatCommand = "iostat"
 
 	intervalStr := strconv.Itoa(interval)
-
 	cmd := exec.Command(IOStatCommand, "-w", intervalStr)
+	return &Collector{cmd: cmd, sender: sender}
+}
 
-	outPipe, _ := cmd.StdoutPipe()
-	err := cmd.Start()
+// Collect function will collect information from iostat
+func (collector *Collector) Collect() error {
+
+	outPipe, _ := collector.cmd.StdoutPipe()
+	err := collector.cmd.Start()
 	if err != nil {
 		return err
 	}
 
-	fmt.Println("process running on PID = ", cmd.Process.Pid)
-
-	// time.AfterFunc(10*time.Second, func() {
-	// 	err = cmd.Process.Kill()
-	// 	if err != nil {
-	// 		fmt.Println(err)
-	// 	}
-	// 	fmt.Println("kill process")
-	// })
+	fmt.Println("process running on PID = ", collector.cmd.Process.Pid)
 
 	reader := bufio.NewReader(outPipe)
 
@@ -45,23 +47,35 @@ func Collect(sender Sender, interval int) error {
 			}
 		}
 
-		cpuStat := Parse(line)
+		cpuStat := parse(line)
 		if cpuStat != nil {
 			payload, err := json.Marshal(cpuStat)
 			if err != nil {
 				fmt.Println("error marshal data : ", err.Error())
 			}
 
-			if err := sender.Send(payload); err != nil {
+			if err := collector.sender.Send(payload); err != nil {
 				return err
 			}
 		}
 	}
 
-	err = cmd.Wait()
+	err = collector.cmd.Wait()
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+// Kill function will kill cmd process
+func (collector *Collector) Kill() error {
+	// time.AfterFunc(10*time.Second, func() {
+	// 	err = cmd.Process.Kill()
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	}
+	// 	fmt.Println("kill process")
+	// })
+	return collector.cmd.Process.Kill()
 }
